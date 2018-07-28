dcbee4e593b35246332741733656c19bb3c2c534
André Luiz Mendonça <andreluzz@gmail.com>
2018-07-28 14:28:09 -0300

    Refactor install script

--- install.sh
+++ install.sh
@@ -2,18 +2,21 @@
 echo "[Hydra Cluster Monitor] Installing"
 
 #copy python lib
+rm $GOPATH/bin/execCommand.py
 cd $GOPATH/src/github.com/hydra-cluster/monitor/lib
 cp -n execCommand.py $GOPATH/bin/
 
-echo "Building Monitor Agent..."
+echo "   Building Monitor Agent..."
 
 #build and install server and agent executables
+rm $GOPATH/bin/monitor-agent
 cd $GOPATH/src/github.com/hydra-cluster/monitor/cmd/agent
 go build -ldflags "-X main.libFolder=/hydra/storage/local/go/bin/" -o monitor-agent
 cp -n monitor-agent $GOPATH/bin/
 
-echo "Building Monitor Server..."
+echo "   Building Monitor Server..."
 
+rm $GOPATH/bin/monitor-server
 cd $GOPATH/src/github.com/hydra-cluster/monitor/cmd/server
 go build -ldflags "-X main.libFolder=/hydra/storage/local/go/bin/" -o monitor-server
 cp -n monitor-server $GOPATH/bin/
