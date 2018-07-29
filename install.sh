d093ae0aefd4622858d048303d892a3a597e945e
André Luiz Mendonça <andreluzz@gmail.com>
2018-07-28 22:25:33 -0300

    init.d scripts

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "[Hydra Cluster Monitor] Installing"
 
 #copy python lib
-sudo rm /usr/local/bin/execCommand.py
+sudo rm -f /usr/local/bin/execCommand.py
 cd $GOPATH/src/github.com/hydra-cluster/monitor/lib
 sudo cp -n execCommand.py /usr/local/bin/
 
@@ -13,23 +13,23 @@ go get -v -t -d ./...
 echo "   Building Monitor Agent..."
 
 #build and install server and agent executables
-sudo rm /usr/local/bin/monitor-agent
+sudo rm -f /usr/local/bin/monitor-agent
 cd $GOPATH/src/github.com/hydra-cluster/monitor/cmd/agent
 go build -ldflags "-X main.libFolder=/usr/local/bin/" -o monitor-agent
 sudo cp -n monitor-agent /usr/local/bin/
 
-sudo rm /etc/init.d/hydra-monitor-agent
+sudo rm -f /etc/init.d/hydra-monitor-agent
 sudo cp -n hydra-monitor-agent /etc/init.d
 sudo chmod +x /etc/init.d/hydra-monitor-agent
 
 echo "   Building Monitor Server..."
 
-sudo rm /usr/local/bin/monitor-server
+sudo rm -f /usr/local/bin/monitor-server
 cd $GOPATH/src/github.com/hydra-cluster/monitor/cmd/server
 go build -ldflags "-X main.libFolder=/usr/local/bin/" -o monitor-server
 sudo cp -n monitor-server /usr/local/bin/
 
-sudo rm /etc/init.d/hydra-monitor-server
+sudo rm -f /etc/init.d/hydra-monitor-server
 sudo cp -n hydra-monitor-server /etc/init.d
 sudo chmod +x /etc/init.d/hydra-monitor-server
 
