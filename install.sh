89d5d90974aba6c21de8f72b4baa8d4366e71275
André Luiz Mendonça <andreluzz@gmail.com>
2018-07-28 22:08:09 -0300

    Install and init.d scripts

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 echo "[Hydra Cluster Monitor] Installing"
 
 #copy python lib
-rm $GOPATH/bin/execCommand.py
+rm /usr/local/bin/execCommand.py
 cd $GOPATH/src/github.com/hydra-cluster/monitor/lib
-cp -n execCommand.py $GOPATH/bin/
+cp -n execCommand.py /usr/local/bin/
 
 echo "   Downloading go dependencies..."
 
@@ -13,10 +13,10 @@ go get -v -t -d ./...
 echo "   Building Monitor Agent..."
 
 #build and install server and agent executables
-rm $GOPATH/bin/monitor-agent
+rm /usr/local/bin/monitor-agent
 cd $GOPATH/src/github.com/hydra-cluster/monitor/cmd/agent
-go build -ldflags "-X main.libFolder=/hydra/storage/local/go/bin/" -o monitor-agent
-cp -n monitor-agent $GOPATH/bin/
+go build -ldflags "-X main.libFolder=/usr/local/bin/" -o monitor-agent
+cp -n monitor-agent /usr/local/bin/
 
 sudo rm /etc/init.d/hydra-monitor-agent
 sudo cp -n hydra-monitor-agent /etc/init.d
@@ -24,10 +24,10 @@ sudo chmod +x /etc/init.d/hydra-monitor-agent
 
 echo "   Building Monitor Server..."
 
-rm $GOPATH/bin/monitor-server
+rm /usr/local/bin/monitor-server
 cd $GOPATH/src/github.com/hydra-cluster/monitor/cmd/server
-go build -ldflags "-X main.libFolder=/hydra/storage/local/go/bin/" -o monitor-server
-cp -n monitor-server $GOPATH/bin/
+go build -ldflags "-X main.libFolder=/usr/local/bin/" -o monitor-server
+cp -n monitor-server /usr/local/bin/
 
 sudo rm /etc/init.d/hydra-monitor-server
 sudo cp -n hydra-monitor-server /etc/init.d
