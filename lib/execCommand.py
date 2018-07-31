d713e6d96a8a4a1f741c31f961d030f671fdf8af
André Luiz Mendonça <andreluzz@gmail.com>
2018-07-31 01:57:00 -0300

    Bug on variables name

--- lib/execCommand.py
+++ lib/execCommand.py
@@ -21,13 +21,13 @@ n = Node()
 if platform.system() == "Linux":
         
     cmd = "uname -r | head --bytes -1"
-    n.kernel = subprocess.check_output(cmd, shell = True )
+    n.Kernel = subprocess.check_output(cmd, shell = True )
 
     cmd = "cat /etc/os-release | head -1 | cut -d '\"' -f 2 | head --bytes -1"
-    n.distro = subprocess.check_output(cmd, shell = True )
+    n.Distro = subprocess.check_output(cmd, shell = True )
 
     cmd = "dmesg | grep Machine | cut -d ':' -f 4 | head --bytes -1"
-    n.model = subprocess.check_output(cmd, shell = True )
+    n.Model = subprocess.check_output(cmd, shell = True )
 
     cmd = "top -bn1 | grep load | awk '{printf \"%.2f\", $(NF-2)}'"
     value = subprocess.check_output(cmd, shell = True )
